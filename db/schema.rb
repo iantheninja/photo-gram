c136f0b0d4f92277ab38fd7c38132bae2a5fb956
iantheninja <munene.ian@live.com>
2016-10-22 11:49:41 +0300

    Posts model and controller added to project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022064441) do
+ActiveRecord::Schema.define(version: 20161022080524) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "descrition"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
