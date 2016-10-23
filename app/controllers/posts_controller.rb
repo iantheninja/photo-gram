f812f9a031368b491e402452ac907a29d15ba97d
iantheninja <munene.ian@live.com>
2016-10-23 21:01:32 +0300

    best css i could do :-(

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,27 @@ class PostsController < ApplicationController
   end
 
   def index
+    @posts = Post.all 
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(permit_post)
+    if @post.save
+      flash[:success] = "Successfully created post"
+      redirect_to posts_path(@post)
+    else
+      flash[:error] = @post.errors.full_messages
+      redirect_to new_post_path
+    end
+  end
+
+  private
+  def permit_post
+    params.require(:post).permit(:image, :descrition)
+
   end
 end
